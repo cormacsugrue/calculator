4c072410b943788a58ebb401f66402eca3fb8202
Cormac <sugrue.cormac@gmail.com>
2024-02-20 14:59:19 +1100

    CREATE function operate
    Operate() takes 2 numeric variables and an operator and returns the
    the result of the equation

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let firstArg = null;
-let secondArg = null;
+let firstNum = null;
+let secondNum = null;
 let operator = null;
 
 
-
+function operate(firstNum, secondNum, operator) {
+    switch (operator) {
+        case 'add':
+            return add(firstNum, secondNum);
+            break;
+        case 'subtract':
+            return subtract(firstNum, secondNum);
+            break;
+        case 'multiply':
+            return multiply(firstNum, secondNum);
+            break;
+        case 'divide':
+            return divide(firstNum, secondNum);
+            break;
+    
+        case null:
+            console.log('The operator is null');
+    }
+}
 
 function add(a,b) {
     return a + b;
